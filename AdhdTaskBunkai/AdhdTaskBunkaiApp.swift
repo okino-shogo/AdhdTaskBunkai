9e69f7a67bd1d5c077c91bc170e52fa05ca2bf2b
okino shogo <okisho0106@gmail.com>
2025-02-17 15:52:41 +0900

    Initial Commit

---
+++ AdhdTaskBunkai/AdhdTaskBunkaiApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdhdTaskBunkaiApp.swift
+//  AdhdTaskBunkai
+//
+//  Created by 沖野匠吾 on 2025/02/17.
+//
+
+import SwiftUI
+
+@main
+struct AdhdTaskBunkaiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
